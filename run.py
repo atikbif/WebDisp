cf1fc28edba313b5e65805a4cea2560d998f23bc
Roman Pelevin <kontel@mail.ru>
2017-08-01 10:57:10 +0300

    Добавлены шаблоны bootstrap

--- run.py
+++ run.py
@@ -4,11 +4,14 @@ from project import app
 from project import db
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask.ext.bootstrap import Bootstrap
 
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+bootstrap = Bootstrap(app)
  
 if __name__ == "__main__":
    manager.run()
